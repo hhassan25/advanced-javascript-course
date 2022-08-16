cc292ec194ea5436797493327266c9b91e327b3f
hadi <ha.hassan2510@hotmail.com>
2022-08-17 00:32:19 +0300

    session 1 changes

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //getting started with advanced javascript course
+
+//session 1 nested function's scope
+let a = 10  //this is the global scope
+function outer(){
+    let b = 20
+    function inner(){
+        let c = 30
+        console.log(a, b, c)
+    }
+    inner()
+}
+outer()
